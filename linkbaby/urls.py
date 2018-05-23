97f43292ee9b93122e4aa65ae63f6d1d36a68b21
Andy Lulham <a.lulham@gmail.com>
2018-05-23 11:35:08 +0100

    Bit of templating

--- linkbaby/urls.py
+++ linkbaby/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import HomeView, CreateView
+from .views import HomeView, eventorganiser_create
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('create', CreateView.as_view(), name='create'),
+    path('create', eventorganiser_create, name='create'),
 ]
--- linkbaby/views.py
+++ linkbaby/views.py
@@ -1,9 +1,33 @@
+from django.forms import modelform_factory
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.views.generic import TemplateView
+# from django.core.mail import send_mail
+
+from .models import EventOrganiser, LinkbabyUser
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
 
-class CreateView(TemplateView):
-    template_name = 'create.html'
+def eventorganiser_create(request):
+    template_name = 'event_organiser_create.html'
+    UserForm = modelform_factory(LinkbabyUser,
+                                 fields=('email',))
+    EventOrganiserForm = modelform_factory(EventOrganiser, exclude=('user',))
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        event_organiser_form = EventOrganiserForm(request.POST)
+        if all((event_organiser_form.is_valid(), user_form.is_valid())):
+            user = user_form.save()
+            event_organiser = event_organiser_form.save(commit=False)
+            event_organiser.user = user
+            event_organiser.save()
+            return HttpResponseRedirect('/')
+    else:
+        event_organiser_form = EventOrganiserForm()
+        user_form = UserForm()
+
+    forms = (event_organiser_form, user_form)
+    return render(request, template_name, {'forms': forms})
