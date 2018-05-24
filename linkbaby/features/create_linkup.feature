d27610654f2aa1b0a6ee19241f6b25d1f8ed9f62
Andy Lulham <a.lulham@gmail.com>
2018-05-24 11:24:20 +0100

    Add a not very good test for linkup creation

--- linkbaby/features/create_linkup.feature
+++ linkbaby/features/create_linkup.feature
@@ -1,11 +1,22 @@
 Feature: Linkup creation
 
-Scenario: Check the form fields
+Scenario: Check linkup creation page works
     When a user visits '/create'
     Then they see a form with the following fields:
-        | label           | type     |
-        | Host name       | Text     |
-        | Email address   | Text     |
-        | Linkup name     | Text     |
-        | Welcome message | Textarea |
-        | Recipients      | Textarea |
+        | label            | type     |
+        | Host name:       | Text     |
+        | Email address:   | Text     |
+        | Linkup name:     | Text     |
+        | Welcome message: | Textarea |
+        | Recipients:      | Textarea |
+
+Scenario: Check linkup creation works
+    When a user visits '/create'
+    And the user submits the following data:
+        | label            | value                                    |
+        | Host name:       | Eve Evans                                |
+        | Email address:   | eve@example.com                          |
+        | Linkup name:     | Thanks for attending Eve’s event         |
+        | Welcome message: | # Hi!\n\nWanted to put you all in touch. |
+        | Recipients:      | Alice <alice@example.com>\nbob@bob.com   |
+    Then the user should be redirected to '/'
--- linkbaby/features/steps/steps.py
+++ linkbaby/features/steps/steps.py
@@ -1,5 +1,6 @@
 from django.test import Client
 from behave import given, when, then
+from bs4 import BeautifulSoup as bs
 
 from linkbaby.factories import LinkupFactory, LinkeeFactory
 from linkbaby.models import Linkup, Linkee, Introduction
@@ -49,8 +50,9 @@ def step_the_total_number_of_introductions_should_be_num(context, num):
 
 @when('a user visits \'{url}\'')
 def step_a_user_visit_url(context, url):
-    client = Client()
-    context.response = client.get(url)
+    context.url = url
+    context.client = Client()
+    context.response = context.client.get(url)
 
 
 @then('they see the text \'{text}\'')
@@ -63,3 +65,22 @@ def step_they_see_a_form_with_fields(context):
     # TODO: field types are currently ignored
     for row in context.table:
         context.test.assertContains(context.response, row['label'])
+
+
+@when('the user submits the following data')
+def step_user_submits_data(context):
+    soup = bs(context.response.content, 'html.parser')
+    url = soup.form.get('action', context.url)
+    payload = {x.get('name'): x.get('value') for x in soup.find_all('input')}
+    payload.update({x.get('name'): x.text for x in soup.find_all('textarea')})
+    for row in context.table:
+        id_ = soup.find('label', text=row['label']).get('for')
+        name = soup.find(id=id_).get('name')
+        payload[name] = row['value']
+
+    context.response = context.client.post(url, data=payload)
+
+
+@then('the user should be redirected to \'{url}\'')
+def step_then_redirected_to(context, url):
+    context.test.assertRedirects(context.response, url)
